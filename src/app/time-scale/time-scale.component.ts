a74eb7e38cdff8f2520b22f48a0b0fb53dbd1450
Jeremy Barr <jrb327@gmail.com>
2017-08-27 13:10:44 -0400

    Add functionality to select data from specific sensors.
    Added buttons to select specific sensors and display data from those
    sensors.

--- src/app/time-scale/time-scale.component.ts
+++ src/app/time-scale/time-scale.component.ts
@@ -11,7 +11,7 @@ import { TimeScaleValue } from './time-scale-value'
 })
 export class TimeScaleComponent implements OnInit {
 // TODO: add TimeScaleValue class to the 'options' Input value.
-  @Input() options =
+  @Input() timeScale =
     [
       {key: 'Today', value: '5'},
       {key: '24hr',  value: '10'},
@@ -19,38 +19,48 @@ export class TimeScaleComponent implements OnInit {
       {key: 'Month', value: '20'}
     ];
 
-   @Input() dataset = 
+   @Input() sensorData = 
    [
+     { key: 'Temp F', value: 'tempf'},
+     { key: 'Temp C', value: 'tempc'}, 
      { key: 'Altitude Ft', value: 'altitude_ft'},
      { key: 'Altitude M', value: 'altitude_m'},
-     { key: 'Temp C', value: 'tempc'},
-     { key: 'tempf', value: 'tempf'},
-     { key: 'humidity', value: 'humidity'},
-     { key: 'pressure', value: 'pressure'},
-     { key: 'time', value: 'time'}
-  ]
+     { key: 'Humidity %RH', value: 'humidity'},
+     { key: 'Pressure Pa', value: 'pressure'},
+     { key: 'Uptime sec', value: 'time'}
+    ]
 
   @Output() select: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private timeScaleService: TimeScaleService) {}
 
   timeScaleTitle = 'Time Scale';
-  isOpen: boolean = false;
+  scale = this.timeScale[0].value;
+  sensor = this.sensorData[0].value;
 
   ngOnInit() {
     //this.options = this.timeScaleService.getTimeScaleOptions();
-    console.log(this.options);
+    console.log(this.timeScale);
+    console.log(this.sensorData);
   }
 
-  // Emit dropdown value to the parent component (Weather Component)
-  selectItem(value:string) {
-    // TODO: change 'value' to object/value to include the dataset {'tempf', value}
-    this.select.emit(value);
-    console.log(value);
+  // Function called when Time Scale button is selected
+  selectTimeScale(timeScaleValue:string) {
+    this.scale = timeScaleValue;
+    this.emitValues(this.scale, this.sensor);
+    console.log(this.scale);
   }
 
-  toggleDropdown() {
-    this.isOpen = !this.isOpen;
+  // Function called when Time Scale button is selected
+  selectSensor(sensorValue:string) {
+    this.sensor = sensorValue;
+    this.emitValues(this.scale, this.sensor);
+    console.log(this.sensor);
+  }
+
+  // Called to output the EventEmitter of the time-scale and sensor data values to the parent component
+  emitValues(scaleValue:string, sensorValue:string ) {
+    this.select.emit({scaleValue:scaleValue,sensorValue:sensorValue});
   }
 
 }
